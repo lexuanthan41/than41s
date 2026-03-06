525f501fe27188b80fd91bb7071496b339f1ed46
lexuanthan41 <thanplul0401as@gmail.com>
2026-03-06 14:01:55 +0700

    bài 6.py

---
+++ bài 6.py
@@ -0,0 +1,21 @@
+a = int(input("Nhập a: "))
+b = int(input("Nhập b: "))
+c = int(input("Nhập c: "))
+
+t = 0
+if a > b:
+    t = a
+    a = b
+    b = t
+
+if a > c:
+    t = a
+    a = c
+    c = t
+
+if b > c:
+    t = b
+    b = c
+    c = t
+
+print("Tăng dần:", a, b, c)
