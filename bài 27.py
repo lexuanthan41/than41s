fcbe292ed797c1bcfdce7e8c29cf503f9a26d6c8
lexuanthan41 <thanplul0401as@gmail.com>
2026-03-20 14:39:05 +0700

    bài 27.py

---
+++ bài 27.py
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+
+    int i = 2;
+    while (n > 1) {
+        if (n % i == 0) {
+            printf("%d", i);
+            n /= i;
+            if (n > 1) printf(" * ");
+        } else {
+            i++;
+        }
+    }
+
+    return 0;
+}
